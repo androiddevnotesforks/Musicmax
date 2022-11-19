95a0d91a8afbedce05bdb9b7dc4795cbe438c7c7
Maximillian Leonov <91663102+MaximillianLeonov@users.noreply.github.com>
2022-11-19 23:27:31 +0400

    Implement ArtistMediaStoreDataSource (#82)

---
+++ core/core-mediastore/src/main/kotlin/com/maximillianleonov/musicmax/core/mediastore/source/ArtistMediaStoreDataSource.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2022 Maximillian Leonov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.maximillianleonov.musicmax.core.mediastore.source
+
+import android.content.ContentResolver
+import android.os.Build
+import android.provider.MediaStore
+import com.maximillianleonov.musicmax.core.model.Artist
+import javax.inject.Inject
+
+class ArtistMediaStoreDataSource @Inject constructor(private val contentResolver: ContentResolver) {
+    fun getAll(): List<Artist> {
+        val artists = mutableListOf<Artist>()
+
+        val collection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            MediaStore.Audio.Artists.getContentUri(MediaStore.VOLUME_EXTERNAL)
+        } else {
+            MediaStore.Audio.Artists.EXTERNAL_CONTENT_URI
+        }
+
+        val projection = arrayOf(
+            MediaStore.Audio.Artists._ID,
+            MediaStore.Audio.Artists.ARTIST,
+            MediaStore.Audio.Artists.NUMBER_OF_TRACKS
+        )
+
+        contentResolver.query(
+            collection,
+            projection,
+            null,
+            null,
+            null
+        )?.use { cursor ->
+            val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Artists._ID)
+            val artistColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Artists.ARTIST)
+            val numberOfTracksColumn =
+                cursor.getColumnIndexOrThrow(MediaStore.Audio.Artists.NUMBER_OF_TRACKS)
+
+            while (cursor.moveToNext()) {
+                val id = cursor.getLong(idColumn)
+                val artist = cursor.getString(artistColumn)
+                val numberOfTracks = cursor.getInt(numberOfTracksColumn)
+
+                artists += Artist(
+                    id = id,
+                    artist = artist,
+                    numberOfSongs = numberOfTracks
+                )
+            }
+        }
+
+        return artists
+    }
+}
