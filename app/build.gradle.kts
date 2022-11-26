a8bdd43d8c8dd59abc49a5d12c9037acb2659c88
Maximillian Leonov <91663102+MaximillianLeonov@users.noreply.github.com>
2022-11-26 16:35:55 +0400

    Update version to 1.0.1 (#143)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
 
     defaultConfig {
         applicationId = "com.maximillianleonov.musicmax"
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 }
 
