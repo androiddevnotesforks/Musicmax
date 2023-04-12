04b2b6f87c2261c5bc0e378399588564eabb8663
Maximillian Leonov <91663102+MaximillianLeonov@users.noreply.github.com>
2023-04-12 21:14:45 +0400

    Replace deprecated `consumedWindowInsets` with new `consumeWindowInsets` (#243)

--- core/core-permission/src/main/kotlin/com/maximillianleonov/musicmax/core/permission/PermissionContent.kt
+++ core/core-permission/src/main/kotlin/com/maximillianleonov/musicmax/core/permission/PermissionContent.kt
@@ -25,7 +25,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.consumedWindowInsets
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -77,7 +77,7 @@ fun PermissionContent(
         Column(
             modifier = Modifier
                 .padding(innerPadding)
-                .consumedWindowInsets(innerPadding)
+                .consumeWindowInsets(innerPadding)
                 .padding(MaterialTheme.spacing.extraMedium)
                 .fillMaxSize(),
             verticalArrangement = Arrangement.Center
