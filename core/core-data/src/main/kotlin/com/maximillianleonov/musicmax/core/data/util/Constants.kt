5e8467aa168599a9a9eccc7eeb5f5ff191b95677
Maximillian Leonov <91663102+MaximillianLeonov@users.noreply.github.com>
2023-04-15 15:26:46 +0400

    Change app settings urls to new ones (#252)
    * Change musicmax page url
    
    * Change privacy policy url
    
    * Change musicmax repository url

--- core/core-data/src/main/kotlin/com/maximillianleonov/musicmax/core/data/util/Constants.kt
+++ core/core-data/src/main/kotlin/com/maximillianleonov/musicmax/core/data/util/Constants.kt
@@ -18,8 +18,8 @@ package com.maximillianleonov.musicmax.core.data.util
 
 internal object Constants {
     internal object Urls {
-        private const val MUSICMAX_PAGE_URL = "https://maximillianleonov.github.io/Musicmax"
-        internal const val MUSICMAX_REPO_URL = "https://github.com/MaximillianLeonov/Musicmax"
-        internal const val PRIVACY_POLICY_URL = "$MUSICMAX_PAGE_URL/privacy-policy.html"
+        private const val MUSICMAX_PAGE_URL = "https://themusicmax.github.io/"
+        internal const val MUSICMAX_REPO_URL = "https://github.com/TheMusicmax/Musicmax"
+        internal const val PRIVACY_POLICY_URL = "$MUSICMAX_PAGE_URL/privacy.html"
     }
 }
