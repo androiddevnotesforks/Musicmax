082fc819384fdeaed445ce1212f7127f2b9b0dd3
Maximillian Leonov <91663102+MaximillianLeonov@users.noreply.github.com>
2023-04-21 14:37:58 +0400

    Fix musicmax page url (#280)

--- core/core-data/src/main/kotlin/com/maximillianleonov/musicmax/core/data/util/Constants.kt
+++ core/core-data/src/main/kotlin/com/maximillianleonov/musicmax/core/data/util/Constants.kt
@@ -18,7 +18,7 @@ package com.maximillianleonov.musicmax.core.data.util
 
 internal object Constants {
     internal object Urls {
-        private const val MUSICMAX_PAGE_URL = "https://themusicmax.github.io/"
+        private const val MUSICMAX_PAGE_URL = "https://themusicmax.github.io"
         internal const val MUSICMAX_REPO_URL = "https://github.com/TheMusicmax/Musicmax"
         internal const val PRIVACY_POLICY_URL = "$MUSICMAX_PAGE_URL/privacy.html"
     }
