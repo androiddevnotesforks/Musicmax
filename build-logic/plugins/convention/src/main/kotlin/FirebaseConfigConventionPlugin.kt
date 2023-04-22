1b50e37061fb3eaa19f343703321930cfd022503
Maximillian Leonov <91663102+MaximillianLeonov@users.noreply.github.com>
2023-04-22 18:38:27 +0400

    Configure Firebase Crashlytics for build types (#297)

--- build-logic/plugins/convention/src/main/kotlin/FirebaseConfigConventionPlugin.kt
+++ build-logic/plugins/convention/src/main/kotlin/FirebaseConfigConventionPlugin.kt
@@ -14,9 +14,11 @@
  * limitations under the License.
  */
 
+import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
 import com.maximillianleonov.musicmax.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
 
 class FirebaseConfigConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
@@ -25,6 +27,22 @@ class FirebaseConfigConventionPlugin : Plugin<Project> {
             with(pluginManager) {
                 apply(libs.plugins.google.services.get().pluginId)
                 apply(libs.plugins.firebase.crashlytics.get().pluginId)
+            }
+        }
+
+        extensions.configure<BaseAppModuleExtension> {
+            buildTypes {
+                debug {
+                    manifestPlaceholders["firebase_crashlytics_collection_enabled"] = false
+                }
+
+                release {
+                    manifestPlaceholders["firebase_crashlytics_collection_enabled"] = true
+                }
+
+                getByName("benchmark") {
+                    manifestPlaceholders["firebase_crashlytics_collection_enabled"] = false
+                }
             }
         }
     }
